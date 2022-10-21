4fbe67b0af60bb4969f044b2bb8bda2925d45d11
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-21 12:58:57 +0100

    Add prints and code for multiple shapes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::f64::consts::PI;
+
 mod user_input;
 
+fn square_area(length: f64) -> f64 {
+    length * length
+}
+
+fn circle_area(length: f64) -> f64 {
+    (PI * length * length) / 4.0
+}
+
+fn equilateral_triangle_area(length: f64) -> f64 {
+    (length * length) * (3.0_f64.cbrt() / 4.0)
+}
+
 fn main() {
-    let length: i32 = user_input::get_user_input("Enter length of side: ");
+    let length: f64 = user_input::get_user_input("Enter length of side (for a circle, length is diameter): ");
 
     // print area/volume of every shape
+
+    println!("Square: area - {}", square_area(length));
+    println!("Circle: area - {}", circle_area(length));
+    println!("Equilateral triangle: area - {}", equilateral_triangle_area(length));
+    println!("Cube: area - {}, volume: {}", 1, 2);
+    // sphere
+    // tetrahedron (3D equilateral triangle)
 }
