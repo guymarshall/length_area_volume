62b85bd59d63ff6a63147e0b1551917bc08f2f9c
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-21 12:20:12 +0100

    Add prompt for length

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod user_input;
+
 fn main() {
-    println!("Hello, world!");
+    let length: i32 = user_input::get_user_input("Enter length of side: ");
+
+    // print area/volume of every shape
 }
