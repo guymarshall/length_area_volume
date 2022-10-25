fa7a4b56dcdee40a256342a7cebae7cf43a67a18
Guy Marshall <guymarshall9@gmail.com>
2022-10-25 22:42:38 +0100

    Add tetrahedron formulae

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn circle_area(length: f64) -> f64 {
 }
 
 fn equilateral_triangle_area(length: f64) -> f64 {
-    (length * length) * (3.0_f64.cbrt() / 4.0)
+    (length * length) * (3.0_f64.sqrt() / 4.0)
 }
 
 fn cube_area(length: f64) -> f64 {
@@ -30,6 +30,14 @@ fn sphere_volume(length: f64) -> f64 {
     (4.0 / 3.0) * PI * (length * length * length)
 }
 
+fn tetrahedron_area(length: f64) -> f64 {
+    length * length * (3.0_f64.sqrt())
+}
+
+fn tetrahedron_volume(length: f64) -> f64 {
+    (length * length * length) / (6.0 * 2.0_f64.sqrt())
+}
+
 fn main() {
     let length: f64 = user_input::get_user_input("Enter length of side (for a circle, length is diameter): ");
 
@@ -38,6 +46,6 @@ fn main() {
     println!("Equilateral triangle: area - {}", equilateral_triangle_area(length));
 
     println!("Cube: area - {}, volume: {}", cube_area(length), cube_volume(length));
-    println!("Sphere: area - {}, volume: {}", sphere_area(length), sphere_volume(length))
-    // tetrahedron (3D equilateral triangle)
+    println!("Sphere: area - {}, volume: {}", sphere_area(length), sphere_volume(length));
+    println!("Tetrahedron: area - {}, volume: {}", tetrahedron_area(length), tetrahedron_volume(length));
 }
