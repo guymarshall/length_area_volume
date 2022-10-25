7b1669bb7b94bad615589f659c60bba4f131c74f
Guy Marshall <guymarshall9@gmail.com>
2022-10-25 22:29:14 +0100

    Add sphere formulae

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,14 @@ fn cube_volume(length: f64) -> f64 {
     length * length * length
 }
 
+fn sphere_area(length: f64) -> f64 {
+    4.0 * PI * (length * length)
+}
+
+fn sphere_volume(length: f64) -> f64 {
+    (4.0 / 3.0) * PI * (length * length * length)
+}
+
 fn main() {
     let length: f64 = user_input::get_user_input("Enter length of side (for a circle, length is diameter): ");
 
@@ -30,6 +38,6 @@ fn main() {
     println!("Equilateral triangle: area - {}", equilateral_triangle_area(length));
 
     println!("Cube: area - {}, volume: {}", cube_area(length), cube_volume(length));
-    // sphere
+    println!("Sphere: area - {}, volume: {}", sphere_area(length), sphere_volume(length))
     // tetrahedron (3D equilateral triangle)
 }
